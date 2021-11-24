dcf8861202f788a258fb6f30c1bfd38b1f865789
Grischa Erbe <grischa.erbe@googlemail.com>
2021-11-24 15:43:01 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# Statamic Enhanced Gql
+
+## Features
+
+This addon provides more explicit queries for Statamic Pro.
+
+- Native GraphQL Queries for collection entries with a single blueprint
+- No Interfaces
+- Pagination
+
+## How to Install
+
+``` bash
+composer require legrisch/statamic-enhanced-graphql
+```
+
+## TODO
+
+- [ ] filter on entries query
+- [ ] queries for entries in collections with more than one blueprint
---
+++ src/ServiceProvider.php
@@ -0,0 +1,117 @@
+<?php
+
+namespace Legrisch\StatamicEnhancedGraphql;
+
+use Statamic\Facades\Collection;
+use Statamic\Providers\AddonServiceProvider;
+use Statamic\Facades\GraphQL;
+use Statamic\GraphQL\Types\EntryType;
+use Facades\Statamic\GraphQL\TypeRegistrar;
+use Statamic\GraphQL\Queries\EntriesQuery;
+use Statamic\GraphQL\Queries\EntryQuery;
+use Illuminate\Pagination\Paginator;
+use Rebing\GraphQL\Support\Facades\GraphQL as FacadesGraphQL;
+use Statamic\Support\Str;
+
+class EnhancedGraphQlServiceProvider extends AddonServiceProvider
+{
+    private static function buildQueryName(\Statamic\Contracts\Entries\Collection $collection, \Statamic\Fields\Blueprint $blueprint, bool $isCollection, bool $hasMultipleBlueprints) {
+        if ($hasMultipleBlueprints) {
+            if ($isCollection) {
+                return $collection->handle() . Str::studly($blueprint->handle()) . 'Entries';
+            } else {
+                return $collection->handle() . Str::studly($blueprint->handle()) . 'Entry';
+            }
+        } else {
+            if ($isCollection) {
+                return $collection->handle() . 'Entries';
+            } else {
+                return $collection->handle() . 'Entry';
+            }
+        }
+    }
+
+    private function addCollectionQueries(): void
+    {
+        $collections = Collection::all();
+        $collections->each(function ($collection) {
+            $blueprints = $collection->entryBlueprints();
+            if ($blueprints->count() > 1) {
+                // TODO: add support for multiple blueprints
+            } else {
+                $typeName = EntryType::buildName($collection, $blueprints->first());
+                $queryName = static::buildQueryName($collection, $blueprints->first(), true, false);
+                $paginationTypeName = $typeName . 'Pagination';
+
+                $args = (new EntriesQuery())->args();
+                unset($args['collection']);
+
+                // TODO: add support for filter
+                unset($args['filter']);
+
+                GraphQL::addQuery([
+                    'name' => $queryName,
+                    'type' => function() use ($typeName, $paginationTypeName) {
+                        return FacadesGraphQL::paginate(GraphQL::type("$typeName!"), $paginationTypeName);
+                    },
+                    'args' => $args,
+                    'resolve' => function ($root, $args) use ($collection) {
+                        $q = new EntriesQuery();
+                        $args['collection'] = [$collection->handle()];
+                        Paginator::currentPageResolver(function () use ($args) {
+                            return $args['page'] ?? 1;
+                        });
+                        return $q->resolve($root, $args);
+                    },
+                ]);
+            }
+        });
+    }
+
+    private function addEntryQueries(): void
+    {
+        $collections = Collection::all();
+        $collections->each(function ($collection) {
+            $blueprints = $collection->entryBlueprints();
+            if ($blueprints->count() > 1) {
+                // TODO: add support for multiple blueprints
+            } else {
+                $typeName = EntryType::buildName($collection, $blueprints->first());
+                $queryName = static::buildQueryName($collection, $blueprints->first(), false, false);
+
+                $args = (new EntryQuery())->args();
+                unset($args['collection']);
+
+                GraphQL::addQuery([
+                    'name' => $queryName,
+                    'type' => function() use ($typeName) {
+                        return GraphQL::type("$typeName");
+                    },
+                    'args' => $args,
+                    'resolve' => function ($root, $args) use ($collection) {
+                        $q = new EntryQuery();
+                        $args['collection'] = $collection->handle();
+                        return $q->resolve($root, $args);
+                    },
+                ]);
+            }
+        });
+    }
+
+    private function registerTypes(): void
+    {
+        TypeRegistrar::register();
+    }
+
+    private function addQueries(): void
+    {    
+        $this->addCollectionQueries();
+        $this->addEntryQueries();
+    }
+
+    public function bootAddon()
+    {
+        $this->registerTypes();
+        $this->addQueries();
+    }
+}
